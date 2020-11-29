16e8fd4fdc9ac3a126ae85d6777f5f467f19bad8
Edition C <editionc18@gmail.com>
2020-11-29 17:29:11 +0900

    데이터 레이어 모델 추가

---
+++ lib/data/models/doc_list_item_model.dart
@@ -0,0 +1,19 @@
+import 'package:meeco_app/domain/entities/doc_list_item.dart';
+
+class DocListItemModel extends DocListItem {
+  DocListItemModel({
+    String query,
+    String title,
+    String author,
+    int view,
+    int comment,
+    int vote,
+  }) : super(
+          query: query,
+          title: title,
+          author: author,
+          view: view,
+          comment: comment,
+          vote: vote,
+        );
+}
