e754d124fd8936198dfe9dbd711e3f311a3150f5
Edition C <editionc18@gmail.com>
2020-11-29 16:54:23 +0900

    mainbox를 mainPage에 추가

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:meeco_app/presentation/widgets/main_box.dart';
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -29,51 +31,71 @@ class _MainPageState extends State<MainPage> {
           ),
         ],
       ),
-      body: ListView(
-        shrinkWrap: true,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '미니기기 코리아',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '미니기기 코리아',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          // ListView(
-          //   scrollDirection: Axis.horizontal,
-          //   shrinkWrap: true,
-          //   children: [
-          //     Container(
-          //       color: Colors.blue,
-          //       height: 150,
-          //       width: 100,
-          //     ),
-          //     Container(
-          //       color: Colors.green,
-          //       height: 150,
-          //       width: 100,
-          //     ),
-          //     Container(
-          //       color: Colors.red,
-          //       height: 150,
-          //       width: 100,
-          //     ),
-          //   ],
-          // ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '메뉴',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 28.0,
+            Container(
+              height: 260,
+              child: PageView(
+                controller: PageController(
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                ),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MainBox(
+                      title: 'IT+',
+                      colors: [
+                        Color(0xFF647cab),
+                        Color(0xFF647cab),
+                        // Color(0xFF4d5f87),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MainBox(
+                      title: '자유+',
+                      colors: [
+                        Color(0xff69b395),
+                        Color(0xff69b395),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MainBox(title: '모아보기', colors: [
+                      Color(0xfffa7570),
+                      Color(0xfffa7570),
+                    ]),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '메뉴',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
