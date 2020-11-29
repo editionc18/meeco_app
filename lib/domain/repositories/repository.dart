0239034f5deb3f3aa798b8ddc5a6c699ef159cc5
Edition C <editionc18@gmail.com>
2020-11-29 17:21:21 +0900

    저장소 클래스 구현

---
+++ lib/domain/repositories/repository.dart
@@ -0,0 +1,13 @@
+import 'package:meeco_app/data/models/response.dart';
+
+import 'package:meeco_app/domain/entities/doc_list.dart';
+import 'package:meeco_app/domain/entities/doc_list_item.dart';
+import 'package:meeco_app/domain/entities/document.dart';
+
+abstract class Repository {
+  Future<Response> login(String id, String pw);
+  void logout();
+
+  Future<Document> getDoc(String query);
+  Future<DocList> getDocList(String query);
+}
