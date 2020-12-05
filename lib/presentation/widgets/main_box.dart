a666d6fa08ba2c2f1ced637bdc619b9185dad22c
Edition C <editionc18@gmail.com>
2020-12-05 21:00:02 +0900

    패딩 변경, 글자 밑에 8픽셀 추가

--- lib/presentation/widgets/main_box.dart
+++ lib/presentation/widgets/main_box.dart
@@ -13,25 +13,27 @@ class MainBox extends StatefulWidget {
 class _MainBoxState extends State<MainBox> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(12.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: widget.colors ??
-                [
-                  Colors.red,
-                  Colors.orange,
-                ],
-            stops: [0.65, 1.0]),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Text(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: widget.colors ??
+                  [
+                    Colors.red,
+                    Colors.orange,
+                  ],
+              stops: [0.65, 1.0]),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
               widget.title,
               style: TextStyle(
                 fontSize: 28.0,
@@ -39,12 +41,15 @@ class _MainBoxState extends State<MainBox> {
                 color: Colors.white,
               ),
             ),
-          ),
-          MainBoxItem(title: '제목'),
-          MainBoxItem(title: '제목제목'),
-          MainBoxItem(title: '미코미코미'),
-          MainBoxItem(title: '제목제목제목'),
-        ],
+            SizedBox(
+              height: 8.0,
+            ),
+            MainBoxItem(title: '제목'),
+            MainBoxItem(title: '제목제목'),
+            MainBoxItem(title: '미코미코미'),
+            MainBoxItem(title: '제목제목제목'),
+          ],
+        ),
       ),
     );
   }
