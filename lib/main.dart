cf5f4d736924bb00af4ba4ab1f4420711135b6fa
Edition C <editionc18@gmail.com>
2020-12-05 20:50:12 +0900

    blocProvider로 감쌈

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:meeco_app/presentation/pages/main_page.dart';
+import 'package:meeco_app/presentation/bloc/tabs/tabs_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    BlocProvider<TabsBloc>(
+      create: (context) => TabsBloc(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
