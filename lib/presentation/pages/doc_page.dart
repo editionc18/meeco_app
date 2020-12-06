f87cc279daf9263dd15344846f68c1e2d193aea3
Edition C <editionc18@gmail.com>
2020-12-06 14:02:11 +0900

    docPage 추가

---
+++ lib/presentation/pages/doc_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DocPage extends StatefulWidget {
+  @override
+  _DocPageState createState() => _DocPageState();
+}
+
+class _DocPageState extends State<DocPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('IT+'),
+      ),
+      body: Container(),
+    );
+  }
+}
