fff1e8ccf9abe184ea8c8aebdd0cc37990b5a067
Edition C <editionc18@gmail.com>
2020-12-06 13:52:32 +0900

    notiitem 기본 형태 만듬

---
+++ lib/presentation/widgets/noti_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NotiItem extends StatefulWidget {
+  @override
+  _NotiItemState createState() => _NotiItemState();
+}
+
+class _NotiItemState extends State<NotiItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(),
+    );
+  }
+}
