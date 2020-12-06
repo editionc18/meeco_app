ed88fa8c865f235253863a41e927447bbeb2305f
Edition C <editionc18@gmail.com>
2020-12-06 15:04:14 +0900

    테스트용 가짜 데이터 리턴하게 함

--- lib/data/datasources/remote_data_source.dart
+++ lib/data/datasources/remote_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:html/parser.dart' show parse;
+import 'package:meeco_app/data/models/doc_list_item_model.dart';
 
 import 'package:meeco_app/data/models/response.dart';
 
@@ -17,9 +18,35 @@ class RemoteDataSourceImpl implements RemoteDataSource {
 
   @override
   Future<DocListModel> getDocList(String query) async {
-    var response = await meecoClient.get(query);
-    var document = parse(response.body);
+    final Response response = await meecoClient.get(query);
+    final document = parse(response.body);
     var list = document.querySelector('#bBd > div.bBox > div');
+    return DocListModel(name: 'IT+', docListItems: [
+      DocListItemModel(
+        query: '/ITplus/202020202',
+        title: '${list.text}',
+        author: '김따란',
+        view: 100,
+        comment: 10,
+        vote: 1,
+      ),
+      DocListItemModel(
+        query: '/ITplus/202020202',
+        title: '제목 테스트',
+        author: '김따란',
+        view: 100,
+        comment: 10,
+        vote: 1,
+      ),
+      DocListItemModel(
+        query: '/ITplus/202020202',
+        title: '제목 테스트',
+        author: '김따란',
+        view: 100,
+        comment: 10,
+        vote: 1,
+      ),
+    ]);
     // return list.text;
   }
 }
