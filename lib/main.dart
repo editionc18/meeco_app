14df9ca3f05866c69172fc06f04255925a3e9e1c
Edition C <editionc18@gmail.com>
2020-12-06 15:43:24 +0900

    blocProvider 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meeco_app/data/datasources/remote_data_source.dart';
+import 'package:meeco_app/data/repositories/repository_impl.dart';
+import 'package:meeco_app/domain/repositories/repository.dart';
+import 'package:meeco_app/domain/usecases/get_doc_list.dart';
 
 import 'package:meeco_app/presentation/pages/main_page.dart';
+import 'package:meeco_app/presentation/bloc/doc_list/doc_list_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  Repository _repository = RepositoryImpl(remote: RemoteDataSourceImpl());
+  runApp(BlocProvider(
+    create: (context) => DocListBloc(
+      getDocList: GetDocList(_repository),
+    ),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
